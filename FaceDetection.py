272e6713ce19ff14b63bfa106bb6c08f440b4933
kimia <kimiyastudent@gmail.com>
2025-05-03 09:56:47 +0330

    loading image

--- FaceDetection.py
+++ FaceDetection.py
@@ -3,3 +3,10 @@ import numpy as np
 import os
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+image_path = "image6.jpg"
+image = cv2.imread(image_path)
+
+if image is None:
+    print("Error! Check the path")
+    exit()
