cc686463711d6de67513dbb950a8b68a33f5155c
Baharfgh <fbghofrani@yahoo.com>
2025-05-08 12:08:53 +0330

    preprocessing

--- FaceDetection.py
+++ FaceDetection.py
@@ -10,3 +10,7 @@ image = cv2.imread(image_path)
 if image is None:
     print("Error! Check the path")
     exit()
+
+image = cv2.GaussianBlur(image, (5, 5), 0)
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray_image = cv2.equalizeHist(gray_image)
